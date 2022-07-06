80e60c7242caa21fec3fd794f375180e05c50f03
Rhileilanto <leah79675@gmail.com>
2022-07-06 08:25:50 +0300

    admindashboard

--- webStartingTemplate/webStartingTemplate/students.php
+++ webStartingTemplate/webStartingTemplate/students.php
@@ -1,48 +1,23 @@
+<?php
+require_once('logics/dbconnection.php');
+$sqlQuery = mysqli_query($conn,"SELECT * FROM enrollment");
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Bootstrap Admin Template</title>
-	<meta charset="UTF-8">
-	<meta name="description" content="Creating admin dashboard">
-	<meta name="keywords" content="HTML,CSS,Zalego,Technology,Zalego institute,JavaScript">
-	<meta name="author" content="Your Name">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="style.css">
+    <?php  require_once('includes/headers.php')?> 
 </head>
 <body>
 	<!-- All our code. write here   -->
-	<div class="header">
-		<img src="" alt="Zalego" height="50" width="50" class="rounded-circle">
-		<a href="#" class="navbar-trigger"><span></span></a>
-	</div>
+	<?php  require_once('includes/navbar.php')?>
+	
 	<div class="sidebar">
-		<nav>
-			<ul>
-				<li>
-					<a href="">
-						<span> <i class="fa fa-group"></i></span>
-						<span>Students</span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<span> <i class="fa fa-folder-open"></i></span>
-						<span>Courses</span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<span> <i class="fa fa-graduation-cap"></i></span>
-						<span>Campus</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
-
+	<?php require_once('includes/sidebar.php')?>
 	</div>
 	<div class="main-content">
+
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12">
@@ -61,7 +36,7 @@
 						<span class="float-right">00</span>
 					</div>
 					<div class="card-footer"></div>
-                    <table class="table table-striped table-hover table-responsive"></table>
+                    <table class="table table-striped table-hover table-responsive">
                         <thead>
                             <tr>
                                 <th>No</th>
@@ -71,23 +46,57 @@
                                 <th>Gender</th>
                                 <th>Course</th>
                                 <th>Enrolled on</th>
+								<th>Actions</th>
                             </tr>
-				
-            <tbody>
-                <?php while($fetch = mysqli_fetch_array($sqlQuery)) { ?>
-                        <tr>
-                            <td><?php echo $fetch['no']?></td>
-                            <td><?php echo $fetch['fullname']?></td>
-                            <td><?php echo $fetch['phonenumber']?></td>
-                            <td><?php echo $fetch['email']?></td>
-                            <td><?php echo $fetch['gender']?></td>
-                            <td><?php echo $fetch['course']?></td>
-                            <td><?php echo $fetch['created_at']?></td>
-                            
-                        </tr>
-                    
-                    <?php } ?>
-                </tbody>
+                        </head> 
+						<tbody>
+							<tr>
+								<td>1.</td>
+								<td>Leah Wanjiru</td>
+								<td>+254713027595</td>
+								<td>leah79675@gmail.com</td>
+								<td>Female</td>
+								<td>Standard</td>
+								<td>23rd August 2022</td>
+								<td>
+									<a href="#" class="btn btn-primary btn-sm">
+										<i class="fa fa-edit"></i>
+									</a>
+									<a href="#" class="btn btn-info btn-sm">
+										<i class="fa fa-eye"></i>
+									</a>
+									<a href="#" class="btn btn-danger btn-sm">
+										<i class="fa fa-trash"></i>
+									</a>
+								</td>
+                            </tr>	
+                        <tbody>
+                    		
+                            <?php while($fetch = mysqli_fetch_array($sqlQuery)) { ?>
+                                    <tr>
+                                        <td><?php echo $fetch['no']?></td>
+                                        <td><?php echo $fetch['fullname']?></td>
+                                        <td><?php echo $fetch['phonenumber']?></td>
+                                        <td><?php echo $fetch['email']?></td>
+                                        <td><?php echo $fetch['gender']?></td>
+                                        <td><?php echo $fetch['course']?></td>
+                                        <td><?php echo $fetch['created_at']?></td>
+                                        
+                                    </tr>
+                                
+							<?php  }?>
+
+                            </tbody>
+					</table>
+				</div>
+              </div>
+            </div>
+        </div>
+
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
             
                 
                     
